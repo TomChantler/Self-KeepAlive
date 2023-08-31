d9fa6a168bb76a549da5031af148a015075f7187
zzhan08 <zzhan08@users.noreply.github.com>
2023-08-31 12:13:06 +0100

    Update self-keepalive.ps1

--- self-keepalive.ps1
+++ self-keepalive.ps1
@@ -4,5 +4,6 @@ $website = (Get-Item Env:WEBSITE_SITE_NAME).value
 $username = "`$${website}"
 $password = (Get-Item Env:CUSTOMCONNSTR_SecretThing).value
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url = "https://${website}.scm.azurewebsites.net/api/diagnostics/runtime"
 $keepalive = Invoke-RestMethod -Uri $url -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method GET
